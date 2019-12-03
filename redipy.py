8376753643b3bc3b993e9522ce566e4a39e9e837
webizfabulous <webizmoxx@gmail.com>
2019-12-03 12:59:42 +0545

    set integer

--- redipy.py
+++ redipy.py
@@ -8,16 +8,14 @@ redis_port = 6379
 
 def hello_redis():
 
-    redisClient = redis.Redis(host=redis_host, port=redis_pord , db=0)
+    redisClient = redis.Redis(host=redis_host, port=redis_port , db=0)
 
     #set a value
     redisClient.set("name", "someman")
 
-    #get a value
-
-    redisClient.get("name")
+    #set a integer
+    redisClient.set("number" ,10)
     
-
 
 hello_redis()
 
