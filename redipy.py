f0cadc1c8cf3590985cb2c169a4fc96a98ccc3da
webizfabulous <webizmoxx@gmail.com>
2019-12-03 13:07:11 +0545

    set redis for some time

--- redipy.py
+++ redipy.py
@@ -1,4 +1,6 @@
 import redis
+import time
+import datetime
 
 
 redis_host = "localhost"
@@ -10,12 +12,20 @@ def hello_redis():
 
     redisClient = redis.Redis(host=redis_host, port=redis_port , db=0)
 
-    #set a value
-    redisClient.set("name", "someman")
+    #set a value for a expiry date
+    date = datetime.timedelta(seconds=10)
 
-    #append a string
-    redisClient.append("name" , "hello")
+    #set a value for 10 seconds
+    redisClient.setex("name" , date,value="someman")
+
+    #print
     print(redisClient.get("name"))
+    #wait for 10 seconds
+    time.sleep(10)
+    #again do the printing
+    print(redisClient.get("name"))
+
+    
 
 
 
